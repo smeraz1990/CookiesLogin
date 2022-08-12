45aadd0372d2e80fc973a8561be28ca9a57ec138
smeraz1990 <82236037+smeraz1990@users.noreply.github.com>
2022-08-11 21:22:17 -0500

    Add files via upload

---
+++ DesafioCookiesLogin/public/script.js
@@ -0,0 +1,13 @@
+fetch("/api/productos-test").then(async(data) => {
+    const response = await data.json();
+    //console.log("session"+response)
+    
+        socket.emit('client:product', response)
+});
+
+ fetch("/").then(async(session) => {
+   
+
+   
+});
+ 
